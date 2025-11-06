a6916662e87c733abfb81a02b17aab39e90691ae
ParthoProtimDas <partho.das@orbitax.com>
2025-11-06 11:27:43 +0600

    generateing new components

---
+++ bot borwser/AIFrontend/src/app/services/data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+
+export interface CardData {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private baseUrl: string = "/api/data";
+  constructor(private http: HttpClient) { }
+
+  getCards(): Observable<CardData[]>{
+    return this.http.get<CardData[]>(`${this.baseUrl}/cards`);
+  }
+  getCardDetails(id: string): Observable<CardData> {
+    return this.http.get<CardData>(`${this.baseUrl}/cards/${id}`);
+  }
+}
